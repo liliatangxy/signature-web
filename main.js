7a2383bbef7688762c4af5e06d23042d15a447cd
Kevin Frans <kevin@bobthechicken.com>
2016-04-30 17:15:59 -0700

    samu

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
 var myvar;
+var name;
 
 function myFunction()
 {
-	var name = $('#input-name').val();
+	name = $('#input-name').val();
 	myvar = window.setInterval(login, 500);
+    $("#login").fadeOut("normal", function() {
+        $(this).remove();
+    });
+    $("#same3").fadeOut("normal", function() {
+        $(this).html("<br>"+name+",<br>Please sign using your phone to login");
+        $(this).fadeIn("normal");
+    });
 
 }
 
 function login()
 {
-	var name = document.getElementById('input-name').value;
-	$.post("http://7ac770f8.ngrok.io/loggedin", {username: "Kevin_Frans"}, function(result){
+	// var name = document.getElementById('input-name').value;
+	$.post("http://7ac770f8.ngrok.io/loggedin", {username: name}, function(result){
         console.log("im back bby" + result);
         if(result == "false")
         {
@@ -24,7 +32,7 @@ function login()
         }
         else
         {
-            swal({title: "Error!",   text: "Incorrect signature for " + name,   type: "error",   confirmButtonText: "Cool" });
+            swal({title: "Error!",   text: "Incorrect signature for " + name + ".",   type: "error",   confirmButtonText: "aw mang" });
         }
     });
 
